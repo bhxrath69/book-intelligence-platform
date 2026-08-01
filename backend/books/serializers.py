6d29a5cea01efc6c79ae859a1eeea915995f3b99
bhxrath69 <bharath20kannan@gmail.com>
2026-08-01 08:45:44 +0530

    chore: serializer updates

--- backend/books/serializers.py
+++ backend/books/serializers.py
@@ -9,13 +9,29 @@ class BookChunkSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class BookListSerializer(serializers.ModelSerializer):
+    """Lightweight serializer for list views - excludes full_text and chunks"""
+    star_display = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Book
+        exclude = ['full_text']
+
+    def get_star_display(self, obj):
+        if obj.rating:
+            stars = int(round(obj.rating))
+            return '*' * stars + '-' * (5 - stars)
+        return '-----'
+
+
 class BookSerializer(serializers.ModelSerializer):
+    """Full serializer for detail views - includes chunks"""
     chunks = BookChunkSerializer(many=True, read_only=True)
     star_display = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
-        fields = '__all__'
+        exclude = ['full_text']
 
     def get_star_display(self, obj):
         if obj.rating:
